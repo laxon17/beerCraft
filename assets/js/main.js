f76a02b9062e00147635bc401a568002a717961a
laxon17 <lazarlalovic@gmail.com>
2022-01-27 23:27:19 +0100

    added beer descrpition, filters are dynamically added from json

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,138 @@
-
-
-async function getData(location) {
+async function fetchData(fileName) {
     try {
-        res = await fetch(location)
-        return await res.json()
+        res = await fetch(`./assets/data/${fileName}.json`)
+        return res.json()
     } catch(err) {
         console.error(err)
     }
 }
 
-function createNavigation() {
+function loadTypeFilters() {
+    const typesList = document.getElementById('typeFilter')
+
+    let typeItems = ''
+    fetchData('types').then(types => {
+        for(let type of types) {
+            typeItems += `
+                <li>
+                    <label>
+                        <input type="checkbox" name="beerType" value="${type.id}" />
+                        <span>${type.name}</span>
+                    </label>
+                </li>
+            `
+        }
+        typesList.innerHTML = typeItems
+    })
+}
+
+function loadBrandFilters() {
+    const brandList = document.getElementById('brandFilter')
+
+    let brandsItems = ''
+    fetchData('breweries').then(brands => {
+        for(let brand of brands) {
+            brandsItems += `
+                <li>
+                    <label>
+                        <input type="checkbox" name="beerBrand" value="${brand.id}" />
+                        <span>${brand.name}</span>
+                    </label>
+                </li>
+            `
+        }
+        brandList.innerHTML = brandsItems
+    })
+}
+
+function loadAlchoholFilters() {
+    const percentageList = document.getElementById('alcFilter')
+
+    let percentageItems = ''
+    fetchData('alc-percentage').then(percentages => {
+        for(let percentage of percentages) {
+            percentageItems += `
+                <li>
+                    <label>
+                        <input type="checkbox" name="beerBrand" value="${percentage.id}" />
+                        <span>${percentage.title}</span>
+                    </label>
+                </li>
+            `
+        }
+        percentageList.innerHTML = percentageItems
+    })
+}
+
+function loadBeers() {
+    const beerContainer = document.getElementById('beerContainer')
+
+    let beerCard = ''
+
+    fetchData('beers').then((beers) => {
+        for(let beer of beers) {
+            beerCard += `
+                <div class="col s12 m6 l3">
+                    <div class="card">
+                        <div class="card-image">
+                            <img class="product-image" src="./assets/img/beers/${beer.beerCover.location}" alt="${beer.beerCover.alternative}">
+                            <a href="#!" class="halfway-fab btn-floating orange"><i class="material-icons">favorite</i></a>
+                        </div>
+                        <div class="card-content">
+                            <span class="card-title activator grey-text text-darken-4">${beer.beerName}<i class="material-icons right">more_vert</i></span>
+                            <p class="small-text pt-1">${loadTypes(beer.types)}</p>
+                            <p class="small-text pt-1">Alc. % ${beer.alcoholPercentage.percentage}</p>
+                            <p id="beer-price" class="pt-2 center">&euro; ${beer.price}</p>
+                        </div>
+                        <div class="center card-action">
+                            <a href="#!">Add To Cart</a>
+                        </div>
+                        <div class="card-reveal">
+                            <span class="card-title grey-text text-darken-4 mb">${beer.beerName}<i class="material-icons right">close</i></span>
+                            <p>
+                                ${beer.beerDescription}
+                            </p>
+                        </div>
+                    </div>
+                </div> 
+            `
+        }
+        beerContainer.innerHTML = beerCard
+    })  
+}
+
+function loadTypes(typeIds) {
+    let output = ''
+    fetchData('types').then(types => {
+            for(let type of types) {
+                for(let typeId in typeIds) {
+                    if(typeIds[typeId] === type.id) {
+                        if(typeId < typeIds.length - 1) {
+                            output += `${type.name}, `
+                        } else output += `${type.name}`
+                    }
+                }
+            }
+            return "OPOOOO"
+    }).catch(err => {
+        console.log(err)
+    })
+}
+
+
+loadBeers()
+scrollToTopBtn()
+loadNavigation()
+loadTypeFilters()
+loadBrandFilters()
+loadAlchoholFilters()
+
+
+function loadNavigation() {    
     const visibleList = document.getElementById('visible-links')
     const collapsedList = document.getElementsByClassName('sideNavCollapsed')[0]
-    getData('./assets/data/navigation.json').then((items) => {
+    
+    fetchData('navigation').then(items => {
         for(let item of items) {
             let listItem = document.createElement('li')
             let linkName = document.createTextNode(item.value)
@@ -22,6 +142,7 @@ function createNavigation() {
                 listItem.append(listLink)
                 collapsedList.append(listItem)
         }
+
         for(let item of items) {
             let listItem = document.createElement('li')
             let linkName = document.createTextNode(item.value)
@@ -31,27 +152,8 @@ function createNavigation() {
                 listItem.append(listLink)
                 visibleList.append(listItem)
         }
-    }).catch((error) => console.error(error))
+    })
 }
-
-createNavigation()
-
-scrollToTopBtn()
-
-// $(window).on('load', () => {
-//     $('#intro').hide()
-//     setTimeout(() => {
-//         $('.loader').animate({
-//             opacity: 0
-//         }, 1000)
-//     }, 2000)
-//     setTimeout(() => {
-//         $('.loader').remove()
-//     }, 3000)
-//     setTimeout(() => {
-//         $('#intro').fadeIn(1000)
-//     },3000)
-// })
 
 function scrollToTopBtn() {
     // BACK TO TOP BUTTON
@@ -78,35 +180,37 @@ function scrollToTopBtn() {
     // END OF BACK TO TOP BUTTON
 }
 
-function loadBeers() {
-    const beerContainer = document.getElementById('beerContainer')
+// $(window).on('load', () => {
+//     $('#intro').hide()
+//     setTimeout(() => {
+//         $('.loader').animate({
+//             opacity: 0
+//         }, 1000)
+//     }, 2000)
+//     setTimeout(() => {
+//         $('.loader').remove()
+//     }, 3000)
+//     setTimeout(() => {
+//         $('#intro').fadeIn(1000)
+//     },3000)
+// })
 
-    let beerCard = ''
-
-    getData('./assets/data/beers.json').then((beers) => {
-        for(let beer of beers) {
-            beerCard += `
-                <div class="col s12 m6 l3">
-                    <div class="card">
-                        <div class="card-image">
-                            <img class="product-image" src="./assets/img/beers/${beer.beerCover.location}" alt="${beer.beerCover.alternative}">
-                            <a href="#!" class="halfway-fab btn-floating orange"><i class="material-icons">favorite</i></a>
-                        </div>
-                        <div class="card-content">
-                            <div class="card-title">${beer.beerName}</div>
-                            <p class="small-text pt-1">IpA</p>
-                            <p class="small-text pt-1">Alc. % ${beer.alcoholPercentage.percentage}</p>
-                            <p id="beer-price" class="pt-2 center">&euro; ${beer.price}</p>
-                        </div>
-                        <div class="center card-action">
-                            <a href="#!">Add To Cart</a>
-                        </div>
-                    </div>
-                </div> 
-            `
-        }
-        beerContainer.innerHTML = beerCard
-    })  
+function clearAllFilters() {
+    document.querySelectorAll('input[type="checkbox"]').forEach(checkbox => {
+        checkbox.checked = false
+    })
+    document.querySelector('input').value = ''
+    document.getElementById('clearFilters').disabled = true
 }
 
-loadBeers()
+document.getElementById('clearFilters').addEventListener('click', () => {
+    if(this.disabled) {}
+    else clearAllFilters()
+})
+
+document.querySelectorAll('input').forEach(checkbox => {
+    const clearBtn = document.getElementById('clearFilters')
+    addEventListener('change', () => {
+        if(clearBtn.disabled) clearBtn.disabled = false 
+    })
+})
