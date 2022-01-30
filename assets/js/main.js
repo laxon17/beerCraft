2e6eec08fe9e5a32f4c851cf3c5c1d9da6adad40
laxon17 <lazarlalovic@gmail.com>
2022-01-30 16:35:26 +0100

    OO

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,75 +2,39 @@ async function fetchData(fileName) {
     try {
         res = await fetch(`./assets/data/${fileName}.json`)
         return res.json()
-    } catch(err) {
-        console.error(err)
+    } catch (error) {
+        console.log(error)
     }
 }
 
-function loadTypeFilters() {
-    const typesList = document.getElementById('typeFilter')
+function loadFilters(dataFile, filterContainer, filterName) {
+    const filterList = document.getElementById(filterContainer)
+    let filterItems = ''
 
-    let typeItems = ''
-    fetchData('types').then(types => {
-        for(let type of types) {
-            typeItems += `
+    fetchData(dataFile).then(items => {
+        for(let item of items) {
+            filterItems += `
                 <li>
                     <label>
-                        <input type="checkbox" name="beerType" value="${type.id}" />
-                        <span>${type.name}</span>
+                        <input type="checkbox" name="${filterName}" value="${item.id}" />
+                        <span>${item.title}</span>
                     </label>
                 </li>
             `
         }
-        typesList.innerHTML = typeItems
+        filterList.innerHTML = filterItems
     })
 }
 
-function loadBrandFilters() {
-    const brandList = document.getElementById('brandFilter')
 
-    let brandsItems = ''
-    fetchData('breweries').then(brands => {
-        for(let brand of brands) {
-            brandsItems += `
-                <li>
-                    <label>
-                        <input type="checkbox" name="beerBrand" value="${brand.id}" />
-                        <span>${brand.name}</span>
-                    </label>
-                </li>
-            `
-        }
-        brandList.innerHTML = brandsItems
-    })
-}
-
-function loadAlchoholFilters() {
-    const percentageList = document.getElementById('alcFilter')
-
-    let percentageItems = ''
-    fetchData('alc-percentage').then(percentages => {
-        for(let percentage of percentages) {
-            percentageItems += `
-                <li>
-                    <label>
-                        <input type="checkbox" name="beerBrand" value="${percentage.id}" />
-                        <span>${percentage.title}</span>
-                    </label>
-                </li>
-            `
-        }
-        percentageList.innerHTML = percentageItems
-    })
-}
 
 function loadBeers() {
     const beerContainer = document.getElementById('beerContainer')
-
     let beerCard = ''
-
+    
     fetchData('beers').then((beers) => {
         for(let beer of beers) {
+            const typesDisplay = 'generate types display'
             beerCard += `
                 <div class="col s12 m6 l3">
                     <div class="card">
@@ -85,7 +49,7 @@ function loadBeers() {
                             <p id="beer-price" class="pt-2 center">&euro; ${beer.price}</p>
                         </div>
                         <div class="center card-action">
-                            <a href="#!">Add To Cart</a>
+                            <a id="addToCart" href="#!">Add To Cart</a>
                         </div>
                         <div class="card-reveal">
                             <span class="card-title grey-text text-darken-4 mb">${beer.beerName}<i class="material-icons right">close</i></span>
@@ -97,103 +61,61 @@ function loadBeers() {
                 </div> 
             `
         }
+
         beerContainer.innerHTML = beerCard
     })  
 }
 
 function loadTypes(typeIds) {
+    let types = fetchData('types')
     let output = ''
-    fetchData('types').then(types => {
-            for(let type of types) {
-                for(let typeId in typeIds) {
-                    if(typeIds[typeId] === type.id) {
-                        if(typeId < typeIds.length - 1) {
-                            output += `${type.name}, `
-                        } else output += `${type.name}`
-                    }
-                }
-            }
-            return "OPOOOO"
-    }).catch(err => {
-        console.log(err)
-    })
+
+    console.log(types)
+
+    return output
 }
 
 
 loadBeers()
 scrollToTopBtn()
 loadNavigation()
-loadTypeFilters()
-loadBrandFilters()
-loadAlchoholFilters()
+loadFilters('breweries', 'brandFilter', 'beerBrand')
+loadFilters('types', 'typeFilter', 'beerType')
+loadFilters('alc-percentage', 'alcFilter', 'beerAlcohol')
 
 
 function loadNavigation() {    
     const visibleList = document.getElementById('visible-links')
     const collapsedList = document.getElementsByClassName('sideNavCollapsed')[0]
     
+    let pagePath = window.location.pathname
+    let pagePathTrimmed = pagePath.substring(1, pagePath.indexOf('.'))
+
     fetchData('navigation').then(items => {
         for(let item of items) {
             let listItem = document.createElement('li')
             let linkName = document.createTextNode(item.value)
             let listLink = document.createElement('a')
-                listLink.href = item.location
+                listLink.href = `${item.location}.html`
                 listLink.append(linkName)
+                pagePathTrimmed === item.location ? listItem.className = 'active' : listItem.className = ''
                 listItem.append(listLink)
                 collapsedList.append(listItem)
         }
-
         for(let item of items) {
             let listItem = document.createElement('li')
             let linkName = document.createTextNode(item.value)
             let listLink = document.createElement('a')
-                listLink.href = item.location
+                listLink.href = `${item.location}.html`
                 listLink.append(linkName)
+                pagePathTrimmed === item.location ? listItem.className = 'active' : listItem.className = ''
                 listItem.append(listLink)
                 visibleList.append(listItem)
         }
     })
 }
 
-function scrollToTopBtn() {
-    // BACK TO TOP BUTTON
-    const backToTopButton = document.getElementById('toTopBtn')
-    const scrollBack = () => window.scrollTo(0, 0)
-
-    backToTopButton.addEventListener('click', scrollBack)
-
-    const showBtn = () => {
-        if(window.pageYOffset > 300) {
-            if(backToTopButton.classList.contains('scale-out')) {
-                backToTopButton.classList.remove('scale-out')
-                backToTopButton.classList.add('scale-in')
-            }
-        } else {
-            if(backToTopButton.classList.contains('scale-in')) {
-                backToTopButton.classList.remove('scale-in')
-                backToTopButton.classList.add('scale-out')    
-            }
-        }
-    }
-
-    window.addEventListener('scroll', showBtn)
-    // END OF BACK TO TOP BUTTON
-}
-
-// $(window).on('load', () => {
-//     $('#intro').hide()
-//     setTimeout(() => {
-//         $('.loader').animate({
-//             opacity: 0
-//         }, 1000)
-//     }, 2000)
-//     setTimeout(() => {
-//         $('.loader').remove()
-//     }, 3000)
-//     setTimeout(() => {
-//         $('#intro').fadeIn(1000)
-//     },3000)
-// })
+// LISTEN IF A FILTER IS SELECTED, AND ENABLE FILTER CLEAR BTN
 
 function clearAllFilters() {
     document.querySelectorAll('input[type="checkbox"]').forEach(checkbox => {
@@ -214,3 +136,50 @@ document.querySelectorAll('input').forEach(checkbox => {
         if(clearBtn.disabled) clearBtn.disabled = false 
     })
 })
+
+// END OF FILTER CLEAR
+
+// INTRO LOADING SCREEN FADEOUT AND REMOVING
+
+$(window).on('load', () => {
+    $('#intro').hide()
+    setTimeout(() => {
+        $('.loader').animate({
+            opacity: 0
+        }, 1000)
+    }, 2000)
+    setTimeout(() => {
+        $('.loader').remove()
+    }, 3000)
+    setTimeout(() => {
+        $('#intro').fadeIn(1000)
+    },3000)
+})
+
+// END OF INTRO LOADING SCREEN FADEOUT AND REMOVING
+
+// BACK TO TOP BUTTON
+
+function scrollToTopBtn() {
+    const backToTopButton = document.getElementById('toTopBtn')
+    const scrollBack = () => window.scrollTo(0, 0)
+
+    backToTopButton.addEventListener('click', scrollBack)
+
+    const showBtn = () => {
+        if(window.pageYOffset > 300) {
+            if(backToTopButton.classList.contains('scale-out')) {
+                backToTopButton.classList.remove('scale-out')
+                backToTopButton.classList.add('scale-in')
+            }
+        } else {
+            if(backToTopButton.classList.contains('scale-in')) {
+                backToTopButton.classList.remove('scale-in')
+                backToTopButton.classList.add('scale-out')    
+            }
+        }
+    }
+    window.addEventListener('scroll', showBtn)
+}
+
+// END OF BACK TO TOP BUTTON
