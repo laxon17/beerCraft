2e7180460397e6ec730b076cd15b22e6bf3a9641
laxon17 <lazarlalovic@gmail.com>
2022-01-30 17:18:00 +0100

    dd

--- assets/js/main.js
+++ assets/js/main.js
@@ -103,7 +103,7 @@ function loadNavigation() {
     const collapsedList = document.getElementsByClassName('sideNavCollapsed')[0]
     
     let pagePath = window.location.pathname
-    let pagePathTrimmed = pagePath.substring(pahePath.indexOf('/', 2), pagePath.indexOf('.'))
+    let pagePathTrimmed = pagePath.substring(pagePath.indexOf('/', 2), pagePath.indexOf('.'))
 
     fetchData('navigation').then(items => {
         for(let item of items) {
